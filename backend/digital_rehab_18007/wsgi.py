6536995c69b802614877c6b7ce52bebfe046f10f
Crowdbotics <team@crowdbotics.com>
2020-06-11 07:43:22 +0000

    Update models

--- backend/digital_rehab_18007/wsgi.py
+++ backend/digital_rehab_18007/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_rehab_18007.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digital_rehab_18007.settings")
 
 application = get_wsgi_application()
